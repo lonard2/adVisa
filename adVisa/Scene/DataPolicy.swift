f2abdc595ce03df4f232ccf231d9d96d8ea15636
Hendra Lijaya <hendralijaya@gmail.com>
2024-08-16 22:16:55 +0700

    Data Policy view completed

---
+++ adVisa/Scene/DataPolicy.swift
@@ -0,0 +1,18 @@
+//
+//  DataPolicy.swift
+//  adVisa
+//
+//  Created by hendra on 16/08/24.
+//
+
+import SwiftUI
+
+struct DataPolicy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataPolicy()
+}
---
+++ adVisa/Scene/DataPolicySheet.swift
@@ -0,0 +1,18 @@
+//
+//  DataPolicySheet.swift
+//  adVisa
+//
+//  Created by hendra on 16/08/24.
+//
+
+import SwiftUI
+
+struct DataPolicySheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataPolicySheet()
+}
