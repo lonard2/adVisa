8776d16d874d480d9c85346d405be59118e2dc98
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-08-18 22:11:23 +0700

    Reenable camera in Discover tab view

--- adVisa/Scene/View/ContentView.swift
+++ adVisa/Scene/View/ContentView.swift
@@ -14,25 +14,23 @@ struct ContentView: View {
     
     var body: some View {
         
-//        GeometryReader { geo in
-//            if openCamera {
-//                CameraView(image: $cameraVm.currentFrame)
-//            } else {
-//                ZStack {
-//                    VStack {
-//                        Button {
-//                            openCamera = true
-//                        } label: {
-//                            Text("Open camera")
-//                        }
-//                    }
-//                }
-//                .frame(width: geo.size.width, height: geo.size.height)
-//            }
-//        }
-        
         TabView {
-            Text("Discover Page")
+            GeometryReader { geo in
+                if openCamera {
+                    CameraView(image: $cameraVm.currentFrame)
+                } else {
+                    ZStack {
+                        VStack {
+                            Button {
+                                openCamera = true
+                            } label: {
+                                Text("Open camera")
+                            }
+                        }
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height)
+                }
+            }
                 .tabItem {
                     Label("Discover", systemImage: "map.circle.fill")
                 }
