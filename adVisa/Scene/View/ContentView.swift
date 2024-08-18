4f3fb9bbb47b7533ef0ba1941c4c05373d6d9399
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-18 20:30:35 +0700

    feat: make tab bar

--- adVisa/Scene/View/ContentView.swift
+++ adVisa/Scene/View/ContentView.swift
@@ -14,21 +14,41 @@ struct ContentView: View {
     
     var body: some View {
         
-        GeometryReader { geo in
-            if openCamera {
-                CameraView(image: $cameraVm.currentFrame)
-            } else {
-                ZStack {
-                    VStack {
-                        Button {
-                            openCamera = true
-                        } label: {
-                            Text("Open camera")
-                        }
-                    }
+//        GeometryReader { geo in
+//            if openCamera {
+//                CameraView(image: $cameraVm.currentFrame)
+//            } else {
+//                ZStack {
+//                    VStack {
+//                        Button {
+//                            openCamera = true
+//                        } label: {
+//                            Text("Open camera")
+//                        }
+//                    }
+//                }
+//                .frame(width: geo.size.width, height: geo.size.height)
+//            }
+//        }
+        
+        TabView {
+            Text("Discover Page")
+                .tabItem {
+                    Label("Discover", systemImage: "map.circle.fill")
                 }
-                .frame(width: geo.size.width, height: geo.size.height)
-            }
+            
+            Text("History Page")
+                .tabItem {
+                    Label("History", systemImage: "clock.fill")
+                }
+            
+            
+            Text("Saved Document Page")
+                .tabItem {
+                    Label("Discover", systemImage: "doc")
+                }
+            
+            
         }
     }
 }
