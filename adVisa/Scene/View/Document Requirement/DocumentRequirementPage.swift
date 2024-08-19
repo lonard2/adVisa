ba3cadf8fcd361c6f684fbfe0f50c5a99763f428
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-19 23:31:37 +0700

    chore: add files and assets

---
+++ adVisa/Scene/View/Document Requirement/DocumentRequirementPage.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentRequirementPage.swift
+//  adVisa
+//
+//  Created by Dixon Willow on 19/08/24.
+//
+
+import SwiftUI
+
+struct DocumentRequirementPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentRequirementPage()
+}
