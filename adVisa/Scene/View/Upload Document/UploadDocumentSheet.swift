f7f9867fc7804e6e6e521aa68b4b879290673daa
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-20 06:59:08 +0700

    feat: add document upload sheet front-end

--- adVisa/Scene/View/Upload Document/UploadDocumentSheet.swift
+++ adVisa/Scene/View/Upload Document/UploadDocumentSheet.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct UploadDocumentSheet: View {
+    
+    @State var documentName: String
+    @State var documentImageName: String
+    @State var documentExplanation: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 3) {
+            
+            VStack(spacing: 24) {
+                Text(documentName)
+                    .font(.system(size: 22))
+                    .fontWeight(.bold)
+                
+                Image(documentImageName)
+                
+                Text(documentExplanation)
+                    .font(.system(size: 17))
+                    .multilineTextAlignment(.center)
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 32)
+            
+            Spacer()
+            
+            VStack(spacing: 20) {
+                Button {
+                    
+                } label: {
+                    Text("Take Picture")
+                        .padding(.vertical, 7)
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 15))
+                        .foregroundStyle(Color(.primaryWhite))
+                        .background(Color.primaryBlue)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Select From Photos")
+                        .padding(.vertical, 7)
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 15))
+                        .foregroundStyle(Color(.primaryBlue))
+                }
+
+            }
+            .padding(.horizontal, 12)
+            .padding(.vertical, 12)
+            
+        }
+        
     }
 }
 
 #Preview {
-    UploadDocumentSheet()
+    UploadDocumentSheet(documentName: "Passport (Bio Page)", documentImageName: "passport_bio_guide", documentExplanation: "It’s the 2nd page of your Passport. It should be looked like the picture above. Use a solid background to take the photo.")
 }
