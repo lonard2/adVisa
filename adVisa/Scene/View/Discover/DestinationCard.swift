bea3d11276f706cfb786d320672345b6f0f8f70f
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-19 17:15:32 +0700

    feat: discover page front end

---
+++ adVisa/Scene/View/Discover/DestinationCard.swift
@@ -0,0 +1,45 @@
+//
+//  DestinationCard.swift
+//  adVisa
+//
+//  Created by Dixon Willow on 19/08/24.
+//
+
+import SwiftUI
+
+struct DestinationCard: View {
+    
+    @State var countryName: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            VStack {
+                Image("identity_card_guide")
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+            }
+            
+            VStack(alignment:.leading, spacing: 4) {
+                Text(countryName)
+                    .font(.system(size: 15))
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color(.black))
+                
+                Text("1st Most Visited")
+                    .font(.system(size: 11))
+                    .foregroundStyle(Color(.black))
+            }
+            .padding(8)
+            
+        }
+        .background(Color(.white))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(radius: 4, x: 0, y: 1)
+        .frame(width: 132, height: 166)
+    }
+}
+
+#Preview {
+    DestinationCard(countryName: "Japan")
+}
---
+++ adVisa/Scene/View/Discover/DiscoverPage.swift
@@ -0,0 +1,78 @@
+//
+//  DiscoverPage.swift
+//  adVisa
+//
+//  Created by Dixon Willow on 19/08/24.
+//
+
+import SwiftUI
+
+struct DiscoverPage: View {
+    
+    let countries = ["Turkey", "Hongkong", "Japan"]
+    @State private var searchText = ""
+    
+    var searchResults: [String] {
+        return countries.filter { $0.contains(searchText) }
+    }
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0){
+                ZStack{
+                    Image("discover_header")
+                    Text("Prepare peacefully, holiday happily")
+                        .foregroundStyle(Color(.white))
+                        .font(.system(size: 16))
+                        .bold()
+                        .offset(y:-30)
+                }
+                
+                VStack {
+                    NavigationStack {
+//                        List {
+//                            ForEach(searchResults, id: \.self) { country in
+//                                NavigationLink {
+//                                    Text(country)
+//                                } label: {
+//                                    Text(country)
+//                                }
+//                            }
+//                        }
+//                        .listStyle(PlainListStyle())
+                        
+                        VStack(alignment: .leading) {
+                            Text("Popular Destination")
+                                .font(.system(size:25))
+                                .fontWeight(.semibold)
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack {
+                                    ForEach(countries, id: \.self) { country in
+                                        NavigationLink {
+                                            Text(country)
+                                        } label: {
+                                            DestinationCard(countryName: country)
+                                                .padding(.horizontal, 16)
+                                                .padding(.vertical)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 16)
+                        
+                        Spacer()
+                    }
+                    .searchable(text: $searchText, prompt: "Search Country")
+                    .background(Color(.white))
+                }
+            }
+        }
+        .background(Color(.blue))
+    }
+}
+
+#Preview {
+    DiscoverPage()
+}
