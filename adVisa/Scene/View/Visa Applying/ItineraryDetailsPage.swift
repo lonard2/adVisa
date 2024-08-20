abca3dc0544829a0352272bc218462475d0a3768
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-20 14:37:21 +0700

    chore: assets change and add

---
+++ adVisa/Scene/View/Visa Applying/ItineraryDetailsPage.swift
@@ -0,0 +1,18 @@
+//
+//  ItineraryDetailsPage.swift
+//  adVisa
+//
+//  Created by Dixon Willow on 20/08/24.
+//
+
+import SwiftUI
+
+struct ItineraryDetailsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItineraryDetailsPage()
+}
