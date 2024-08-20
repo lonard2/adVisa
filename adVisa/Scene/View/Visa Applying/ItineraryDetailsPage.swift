b82dc473b5fa610eda3717156ddde9abb52fbf7f
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-20 14:44:51 +0700

    feat: create itinerary plan page front-end

--- adVisa/Scene/View/Visa Applying/ItineraryDetailsPage.swift
+++ adVisa/Scene/View/Visa Applying/ItineraryDetailsPage.swift
@@ -9,7 +9,74 @@ import SwiftUI
 
 struct ItineraryDetailsPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            ZStack{
+                Image("visa_header")
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack(spacing: 20) {
+                    Image("step_3")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    VStack(spacing: 8) {
+                        Text("ITINERARY DETAILS")
+                            .foregroundStyle(Color(.primaryWhite))
+                            .font(.system(size: 22))
+                            .bold()
+                        
+                        Text("Tell more about your plans in there")
+                            .foregroundStyle(Color(.primaryWhite))
+                            .font(.system(size: 15))
+                    }
+                }
+                .offset(y: -36)
+            }
+            .background(Color(.primaryBlue))
+            
+            VStack(spacing: 12) {
+                
+                HStack {
+                    Text("Personal Data")
+                        .foregroundStyle(Color(.primaryBlack))
+                        .font(.system(size: 15))
+                        .fontWeight(.semibold)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                        .foregroundStyle(Color(.primaryBlack))
+                        .font(.system(size: 15))
+                    
+                }
+                .padding(16)
+                .background(Color(.primaryWhite))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .shadow(color: .black.opacity(0.25) ,radius: 4)
+                .frame(width: .infinity, height: .infinity)
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("Continue")
+                        .padding(.vertical, 7)
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 15))
+                        .foregroundStyle(Color(.primaryWhite))
+                        .background(Color(.primaryBlue))
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+            }
+            .padding(.vertical, 20)
+            .padding(.horizontal, 16)
+            .background(Color(.primaryWhite))
+            
+            Spacer()
+        }
     }
 }
 
