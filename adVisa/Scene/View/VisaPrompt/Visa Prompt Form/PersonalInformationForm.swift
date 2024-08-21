d120e02979eb7f30a10b0f7cb55e2d0c41db3303
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-21 09:12:08 +0700

    optimize: chunk visa prompt view

---
+++ adVisa/Scene/View/VisaPrompt/Visa Prompt Form/PersonalInformationForm.swift
@@ -0,0 +1,65 @@
+//
+//  PersonalInformationForm.swift
+//  adVisa
+//
+//  Created by Dixon Willow on 21/08/24.
+//
+
+import SwiftUI
+
+struct PersonalInformationForm: View {
+    
+    @ObservedObject var viewModel: VisaPromptViewModel
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Have your ever go to Fukushima/ Miyagi/ Iwate/ Okinawa?")
+                .font(.system(size: 17))
+                .fontWeight(.semibold)
+            
+            Picker(selection: $viewModel.visitedPlaces) {
+                Text("Yes").tag(true)
+                Text("No").tag(false)
+                
+            } label: {
+                Text("visitedPlace")
+            }
+            .pickerStyle(.palette)
+        }
+        
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Are you a student?")
+                .font(.system(size: 17))
+                .fontWeight(.semibold)
+            
+            Picker(selection: $viewModel.isStudent) {
+                Text("Yes").tag(true)
+                Text("No").tag(false)
+                
+            } label: {
+                Text("isStudent")
+            }
+            .pickerStyle(.palette)
+        }
+        
+        Spacer()
+        
+        NavigationLink {
+            
+            
+            
+        } label: {
+            Text("Continue")
+                .padding(.vertical, 7)
+                .frame(maxWidth: .infinity)
+                .font(.system(size: 15))
+                .foregroundStyle(Color(.primaryWhite))
+                .background(Color.primaryBlue)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+        }
+    }
+}
+
+#Preview {
+    PersonalInformationForm(viewModel: VisaPromptViewModel())
+}
