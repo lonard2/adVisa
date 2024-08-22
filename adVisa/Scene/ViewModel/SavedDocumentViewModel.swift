b575513bce74e1f37805c0ddd4d41d076992c11f
Lonard Steven <86178738+lonard2@users.noreply.github.com>
2024-08-22 17:50:51 +0700

    Add tutorial images in saved document & document requirement

--- adVisa/Scene/ViewModel/SavedDocumentViewModel.swift
+++ adVisa/Scene/ViewModel/SavedDocumentViewModel.swift
@@ -10,10 +10,10 @@ import Foundation
 class SavedDocumentViewModel: ObservableObject {
     
     @Published var cardData: [Document] = [
-        Document(icon: "", imageName: "passport_bio_guide.png", documentName: "Passport (Bio Page)", explanation: "It’s the 2nd page of your Passport. It should be looked like the picture above. Use a solid background to take the photo."),
-        Document(icon: "", imageName: "passport_endorsement_guide.png", documentName: "Passport (Endorsement Page)", explanation: "It’s the 4rd page of your Passport. It should be looked like the picture above. Use a solid background to take the photo."),
-        Document(icon: "", imageName: "identity_card_guide.png", documentName: "Identity Card (KTP)", explanation: "Front side picture of your ID Card (KTP). We recommends you to use a solid background. See the photo guide as the picture above."),
-        Document(icon: "", imageName: "portrait_guideline.png", documentName: "Self Portrait", explanation: "A full-color clear image with a solid background. If you want to use an existing image, make sure that it’s taken no longer than 6 months ago. See the photo guide as the picture above.")
+        Document(icon: "", imageName: "passport_bio_guide", documentName: "Passport (Bio Page)", explanation: "It’s the 2nd page of your Passport. It should be looked like the picture above. Use a solid background to take the photo."),
+        Document(icon: "", imageName: "passport_endorsement_guide", documentName: "Passport (Endorsement Page)", explanation: "It’s the 4rd page of your Passport. It should be looked like the picture above. Use a solid background to take the photo."),
+        Document(icon: "", imageName: "identity_card_guide", documentName: "Identity Card (KTP)", explanation: "Front side picture of your ID Card (KTP). We recommends you to use a solid background. See the photo guide as the picture above."),
+        Document(icon: "", imageName: "portrait_guideline", documentName: "Self Portrait", explanation: "A full-color clear image with a solid background. If you want to use an existing image, make sure that it’s taken no longer than 6 months ago. See the photo guide as the picture above.")
     ]
     
 }
