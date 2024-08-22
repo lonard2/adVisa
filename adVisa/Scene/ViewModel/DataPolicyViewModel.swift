ed9bcdd90d687842b93ddfc46bf9653b14245a6f
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-22 16:39:47 +0700

    feat: save agreement to user default

--- adVisa/Scene/ViewModel/DataPolicyViewModel.swift
+++ adVisa/Scene/ViewModel/DataPolicyViewModel.swift
@@ -8,8 +8,23 @@
 import Foundation
 
 class DataPolicyViewModel: ObservableObject {
-    @Published var isAgreeToTerms: Bool = false
-    @Published var isAgreeToPrivacy: Bool = false
+    @Published var isAgreeToTerms: Bool {
+        didSet {
+            UserDefaults.standard.set(isAgreeToTerms, forKey: "isAgreeToTerms")
+        }
+    }
+    
+    @Published var isAgreeToPrivacy: Bool {
+        didSet {
+            UserDefaults.standard.set(isAgreeToPrivacy, forKey: "isAgreeToPrivacy")
+        }
+    }
     
     @Published var showSheet: Bool = true
+    
+    init() {
+        self.isAgreeToTerms = UserDefaults.standard.bool(forKey: "isAgreeToTerms")
+        self.isAgreeToPrivacy = UserDefaults.standard.bool(forKey: "isAgreeToPrivacy")
+        self.showSheet = !isAgreeToTerms || !isAgreeToPrivacy
+    }
 }
