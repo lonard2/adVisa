d7fc548c9bbd32623d98de0dc25718513f2f4b06
Dyxelta <92355144+Dyxelta@users.noreply.github.com>
2024-08-22 20:02:06 +0700

    chore: file adding

---
+++ adVisa/Scene/View/History/HistoryPage.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryPage.swift
+//  adVisa
+//
+//  Created by Dixon Willow on 22/08/24.
+//
+
+import SwiftUI
+
+struct HistoryPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryPage()
+}
