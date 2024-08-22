de9acc516613232bd38de5979efbe005f89a0e3a
Hendra Lijaya <hendralijaya@gmail.com>
2024-08-22 17:11:41 +0700

    Camera View

---
+++ adVisa/Scene/NavigationManager.swift
@@ -0,0 +1,12 @@
+//
+//  NavigationManager.swift
+//  adVisa
+//
+//  Created by hendra on 22/08/24.
+//
+
+import Foundation
+
+class NavigationManager: ObservableObject {
+    @Published var shouldNavigate: Bool = false
+}
